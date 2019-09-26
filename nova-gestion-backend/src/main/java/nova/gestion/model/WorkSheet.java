b20355b91bac2f67b657e086daaf464f7f22085d
massintalah <massintalah@hotmail.com>
2019-09-26 10:28:21 -0400

    added worksheet class

---
+++ nova-gestion-backend/src/main/java/nova/gestion/model/WorkSheet.java
@@ -0,0 +1,17 @@
+package nova.gestion.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.sql.Date;
+
+@Data
+@AllArgsConstructor
+public class WorkSheet {
+    private int idWorkSheet;
+    private int idStatus;
+    private int quantity;
+    private Date dateCreation;
+    private Data dueDate;
+    private String orderNumber;
+}
