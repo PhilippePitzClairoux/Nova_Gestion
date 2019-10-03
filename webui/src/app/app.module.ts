18d485340773e37891a3b4b19adf430a76e52daa
Leccho <leclercc11@gmail.com>
2019-10-03 10:18:13 -0400

    angular material added

--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule  } from '@angular/material';
 
 @NgModule({
   declarations: [
