f820fca8f82b89497af63e7c334f387531e89963
Leccho <leclercc11@gmail.com>
2019-10-04 13:41:56 -0400

    theme for angular material added

--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule  } from '@angular/material';
+import { MatSliderModule, MatMenuModule, MatIconModule, MatSidenavModule, MatButtonModule, MatCardModule  } from '@angular/material';
 
 @NgModule({
   declarations: [
