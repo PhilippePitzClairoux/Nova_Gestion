d1dab3f0183a79884222bcc05aad67f35deebfda
Leccho <leclercc11@gmail.com>
2019-10-05 14:11:57 -0400

    router outlet added

--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { SharedModule } from './shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule, MatMenuModule, MatIconModule, MatSidenavModule, MatButtonModule, MatCardModule  } from '@angular/material';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
