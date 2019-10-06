2dd3bfef1686222c67db7513221c73f8d91bc410
cvilanova <cyvila@gmail.com>
2019-10-06 12:40:44 -0400

    Sidenav

--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './button/button.component';
+import {SidebarComponent} from './navigation/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule
@@ -15,4 +17,5 @@ import { ButtonComponent } from './button/button.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
