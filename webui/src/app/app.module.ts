7cd0f3e3673158124fbe0a31b4cb2e9a25968a84
Leccho <leclercc11@gmail.com>
2019-10-07 17:38:15 -0400

    Angular material and theme (#9)
    * angular material added
    
    * theme for angular material added
    
    * color added for the general theme
    
    * router outlet added
    
    * code review correction
    
    * Fonts + button component (#10)
    
    * created shared module
    
    * moving button component in the shared module
    
    * removed test file for button component
    
    * deleted test file of app component
    
    * style up to date
    
    * removed unuse dependencies
    
    * angular material added
    
    * theme for angular material added
    
    * color added for the general theme
    
    * router outlet added
    
    * code review correction
    
    * deleted test file of app component
    
    * style up to date
    
    * removed unuse dependencies
    
    * Update app.module.ts

--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    AppRoutingModule,
+    SharedModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
