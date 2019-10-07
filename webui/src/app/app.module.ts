50032c3210e035740e2e6c43d56e6df00f96e994
Leccho <leclercc11@gmail.com>
2019-10-07 09:08:43 -0400

    removed unuse dependencies

--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { SharedModule } from './shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
