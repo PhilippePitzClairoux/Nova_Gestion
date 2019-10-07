bee8064bad0374ef84ee78673f4e88a9cbecf457
Leccho <leclercc11@gmail.com>
2019-10-06 22:32:31 -0400

    created user component

--- webui/src/app/users/users.module.ts
+++ webui/src/app/users/users.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersListComponent } from './users-list/users-list.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    UsersListComponent
+    UsersListComponent,
+    UserComponent
   ],
   imports: [
     CommonModule,
