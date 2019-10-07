06b9fcb9b154d44c87f150ad0b6815e4b34590e4
Leccho <leclercc11@gmail.com>
2019-10-06 21:29:31 -0400

    moving button component in the shared module

--- webui/src/app/shared/shared.module.ts
+++ webui/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
+import { ButtonComponent } from './button/button.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
-
 @NgModule({
-  declarations: [],
+  declarations: [
+    ButtonComponent
+  ],
   imports: [
     CommonModule
   ]
