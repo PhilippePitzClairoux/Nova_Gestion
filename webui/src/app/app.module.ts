5cb92d87c287b8f109818a8a8a9afcf9c8c6845f
cvilanova <cyvila@gmail.com>
2019-10-08 14:42:08 -0400

    Delete duplicated imports and missing commas (#17)

--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -4,13 +4,7 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {SidebarComponent} from './navigation/sidebar/sidebar.component';
-import { SharedModule } from './shared/shared.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
@@ -21,7 +15,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     SharedModule,
-    FontAwesomeModule
+    FontAwesomeModule,
     AppRoutingModule,
     SharedModule,
     BrowserAnimationsModule,
