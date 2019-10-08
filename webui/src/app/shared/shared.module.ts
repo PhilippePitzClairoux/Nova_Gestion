7be0f9d8277fdd10a6fd356aab78b3daa9706c92
Leccho <leclercc11@gmail.com>
2019-10-08 17:13:30 -0400

    created header component

--- webui/src/app/shared/shared.module.ts
+++ webui/src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import {ButtonComponent} from './button/button.component';
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    ButtonComponent
+    ButtonComponent,
+    HeaderComponent
   ],
   exports: [
     ButtonComponent
