c88d933be8558b30912a0cb5a07d6621f93ff7b8
cvilanova <cyvila@gmail.com>
2019-10-10 09:20:15 -0400

    Fixed sidenav + using material icons instead of fontawesome

--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -2,11 +2,11 @@ import {SharedModule} from './shared/shared.module';
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {SidebarComponent} from './navigation/sidebar/sidebar.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {InventoryModule} from './inventory/inventory.module';
+import {MatIconModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -16,11 +16,11 @@ import {InventoryModule} from './inventory/inventory.module';
   imports: [
     BrowserModule,
     SharedModule,
-    FontAwesomeModule,
     AppRoutingModule,
     SharedModule,
     BrowserAnimationsModule,
-    InventoryModule
+    InventoryModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- webui/src/app/shared/shared.module.ts
+++ webui/src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {ButtonComponent} from './button/button.component';
-import { HeaderComponent } from './header/header.component';
+import {HeaderComponent} from './header/header.component';
 
 @NgModule({
   declarations: [
