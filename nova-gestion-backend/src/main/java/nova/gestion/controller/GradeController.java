6a155ff3ea3d3159a6d27133b3263cf187ce2a87
massitalah <51333588+massitalah@users.noreply.github.com>
2019-10-16 10:33:17 -0400

    getAllGrades  (#24)
    * getAllGrades fonctionnel
    
    * correction de usertypes

--- nova-gestion-backend/src/main/java/nova/gestion/controller/GradeController.java
+++ nova-gestion-backend/src/main/java/nova/gestion/controller/GradeController.java
@@ -1,8 +1,9 @@
 package nova.gestion.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import nova.gestion.model.Grade;
 import nova.gestion.model.TypeUser;
-import nova.gestion.services.TypeUserService;
+import nova.gestion.services.GradeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,18 +14,17 @@ import java.util.ArrayList;
 
 @RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class GradeController {
 
-public class TypeUserController {
-
-    private final TypeUserService typeUserService;
+    private final GradeService gradeService;
 
     @Autowired
-    public TypeUserController(TypeUserService typeUserService) {
-        this.typeUserService = typeUserService;
+    public GradeController(GradeService gradeService) {
+        this.gradeService = gradeService;
     }
 
-    @GetMapping("/v1/usertypes")
-    public ArrayList<TypeUser> getAllUserTypes() throws JsonProcessingException {
-        return typeUserService.getAllUserTypes();
+    @GetMapping("/v1/grades")
+    public ArrayList<Grade> getAllGrades() throws JsonProcessingException {
+        return gradeService.getAllGrades();
     }
 }
