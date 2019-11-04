76bd7545f414dc007b8ce7e1653a0b2577c46198
Leccho <leclercc11@gmail.com>
2019-11-04 14:24:15 -0500

    get/create/update/delete program db access

--- webui/src/app/services/program.service.ts
+++ webui/src/app/services/program.service.ts
@@ -1,3 +1,6 @@
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Program } from './../models/program.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +8,44 @@ import { Injectable } from '@angular/core';
 })
 export class ProgramService {
 
-  constructor() { }
+  private httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+
+  private programsList: Program[] = [];
+  private programsListSubject: BehaviorSubject<Program[]> = new BehaviorSubject<Program[]>([]);
+
+  constructor(private http: HttpClient) { }
+
+  public programsList$(): Observable<Program[]> {
+    return this.programsListSubject.asObservable();
+  }
+
+  public getAllProgram(): void {
+    this.http.get<Program[]>('/v1/programs').subscribe(result => {
+      this.programsList = result;
+      this.programsListSubject.next(this.programsList);
+    });
+  }
+
+  public createProgram(program: Program): void {
+    this.http.post<Program>('/v1/program', program, this.httpOptions).subscribe(result => {
+      program.idProgram = result.idProgram;
+      this.programsList = [...this.programsList, program];
+      this.programsListSubject.next(this.programsList);
+    });
+  }
+
+  public updateProgram(program: Program): void {
+    this.http.put<Program>('/v1/program', program, this.httpOptions).subscribe(() => {
+      const index = this.programsList.findIndex(t => t.idProgram === program.idProgram);
+      this.programsList[index] = program;
+      this.programsListSubject.next(this.programsList);
+    });
+  }
+
+  public deleteProgram(id: number): void {
+    this.http.delete<Program>('/v1/program/' + id.toString() + '/').subscribe(() => {
+      this.programsList = this.programsList.filter(t => t.idProgram !== id);
+      this.programsListSubject.next(this.programsList);
+    });
+  }
 }
