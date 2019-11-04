09acb025e91a78184f34c027f9b881bbb521ccb9
Philippe Pitz Clairoux <philippepitz123@gmail.com>
2019-11-04 14:01:49 -0500

    added controller to show exceptions in a more..fancy way

---
+++ nova-gestion-backend/src/main/java/nova/gestion/errors/controller/CustomGlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package nova.gestion.errors.controller;
+
+import nova.gestion.errors.exceptions.InvalidRequest;
+import nova.gestion.errors.model.Error;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(InvalidRequest.class)
+    public ResponseEntity<Error> customHandler(Exception ex, WebRequest request) {
+        System.out.println(request);
+        System.out.println(ex.toString());
+        return new ResponseEntity<Error> (Error.builder()
+                .httpStatus(HttpStatus.BAD_REQUEST.value())
+                .exception(ex.getMessage())
+                .errorMessage(ex.getLocalizedMessage())
+                .timestamp(new Date())
+                .build(), HttpStatus.BAD_REQUEST);
+    }
+}
