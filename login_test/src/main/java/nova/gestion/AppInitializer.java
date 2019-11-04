fc630cb90e36cf87e2166a544377d5374f5fff66
Philippe Pitz Clairoux <philippepitz123@gmail.com>
2019-11-04 13:44:56 -0500

    removed useless mini project that's not supposed to be there...

--- login_test/src/main/java/nova/gestion/AppInitializer.java
+++
@@ -1,9 +0,0 @@
-package nova.gestion;
-
-public class AppInitializer
-        extends AbstractAnnotationConfigDispatcherServletInitializer {
-
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[] {SecSecurityConfig.class};
-    }
-}
