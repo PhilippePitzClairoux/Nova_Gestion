d0f21eb4e8d0ddc1a73f411d5e87c65d9bf3af4c
Philippe Pitz Clairoux <philippepitz123@gmail.com>
2019-11-04 14:32:59 -0500

    added some better error handling

--- nova-gestion-backend/src/main/java/nova/gestion/errors/controller/CustomGlobalExceptionHandler.java
+++ nova-gestion-backend/src/main/java/nova/gestion/errors/controller/CustomGlobalExceptionHandler.java
@@ -2,17 +2,23 @@ package nova.gestion.errors.controller;
 
 
 import nova.gestion.errors.model.Error;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
 @ControllerAdvice
-public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+@Controller
+public class CustomGlobalExceptionHandler implements ErrorController {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Error> customHandler(Exception ex, WebRequest request) {
@@ -22,5 +28,21 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
                 .errorMessage(ex.getLocalizedMessage())
                 .timestamp(new Date())
                 .build(), HttpStatus.BAD_REQUEST);
+    }
+
+    @RequestMapping("/error")
+    @ResponseBody
+    public ResponseEntity<Error> handleError(HttpServletRequest request) {
+        return new ResponseEntity<Error> (Error.builder()
+                .httpStatus(HttpStatus.NOT_FOUND.value())
+                .exception("Page not found")
+                .errorMessage("The page you're looking for does not exist")
+                .timestamp(new Date())
+                .build(), HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
     }
 }
