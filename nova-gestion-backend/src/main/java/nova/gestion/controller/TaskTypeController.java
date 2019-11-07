eacd7eb9ee10304f6d4d0995f7ef77e88fd00416
Philippe Pitz Clairoux <philippepitz123@gmail.com>
2019-11-06 19:41:54 -0500

    added getAllTaskTypes controller

---
+++ nova-gestion-backend/src/main/java/nova/gestion/controller/TaskTypeController.java
@@ -0,0 +1,29 @@
+package nova.gestion.controller;
+
+import nova.gestion.model.TaskType;
+import nova.gestion.services.TaskTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class TaskTypeController {
+
+    private final TaskTypeService taskTypeService;
+
+    @Autowired
+    public TaskTypeController(TaskTypeService taskTypeService) {
+        this.taskTypeService = taskTypeService;
+    }
+
+    @GetMapping("/v1/taskTypes")
+    public ArrayList<TaskType> getAllTaskTypes() {
+        return taskTypeService.getAllTaskTypes();
+    }
+
+}
--- nova-gestion-backend/src/main/java/nova/gestion/mappers/TaskTypeMapper.java
+++ nova-gestion-backend/src/main/java/nova/gestion/mappers/TaskTypeMapper.java
@@ -4,10 +4,12 @@ import nova.gestion.model.TaskType;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Mapper
 @Repository
 public interface TaskTypeMapper {
 
     TaskType getTaskTypeById(int idTaskType);
-
+    ArrayList<TaskType> getAllTaskTypes();
 }
