25cb519091b22e9acf67c5dd5efcaeed26b24951
massintalah <massintalah@hotmail.com>
2019-11-11 12:19:48 -0500

    gestion des maintenance: add, update, delete

--- nova-gestion-backend/src/main/java/nova/gestion/mappers/MaintenanceMapper.java
+++ nova-gestion-backend/src/main/java/nova/gestion/mappers/MaintenanceMapper.java
@@ -1,5 +1,6 @@
 package nova.gestion.mappers;
 
+import com.sun.tools.javac.Main;
 import nova.gestion.model.Machine;
 import nova.gestion.model.Maintenance;
 import org.apache.ibatis.annotations.Mapper;
@@ -14,4 +15,10 @@ public interface MaintenanceMapper {
     Maintenance getMaintenance(int idMaintenance);
 
     ArrayList<Maintenance> getMaintenancesByMachine(Machine machine);
+
+    void insertMaintenance(Maintenance maintenance);
+
+    void updateMaintenance(Maintenance maintenance);
+
+    void deleteMaintenance(int idMaintenance);
 }
