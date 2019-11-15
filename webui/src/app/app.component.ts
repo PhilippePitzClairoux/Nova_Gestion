3ef33f0ddfef816e934006d495ffd3e29775bf85
Leccho <leclercc11@gmail.com>
2019-11-15 14:10:43 -0500

    creating page adding style making nav hide on authentification

--- webui/src/app/app.component.ts
+++ webui/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,4 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  public authentification: boolean;
+
+  constructor(public router: Router) {
+  }
 }
--- webui/src/app/authentification/authentification.module.ts
+++ webui/src/app/authentification/authentification.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { AuthentificationRoutingModule } from './authentification-routing.module';
 import { AuthentificationComponent } from './authentification/authentification.component';
+import { MatCardModule } from '@angular/material';
 
 
 @NgModule({
   declarations: [AuthentificationComponent],
   imports: [
     CommonModule,
+    MatCardModule,
     AuthentificationRoutingModule
   ]
 })
