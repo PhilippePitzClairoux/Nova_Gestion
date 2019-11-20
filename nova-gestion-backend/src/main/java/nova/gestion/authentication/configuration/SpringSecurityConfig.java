2b4ec18820b2e3864d989f41646d5131d8bfcde2
Philippe Pitz Clairoux <philippepitz123@gmail.com>
2019-11-19 23:23:33 -0500

    fixed logout errors

--- nova-gestion-backend/src/main/java/nova/gestion/authentication/configuration/SpringSecurityConfig.java
+++ nova-gestion-backend/src/main/java/nova/gestion/authentication/configuration/SpringSecurityConfig.java
@@ -6,6 +6,7 @@ import nova.gestion.authentication.handlers.SavedRequestAwareAuthenticationSucce
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
 
 @Configuration
@@ -50,6 +52,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .failureHandler(new SimpleUrlAuthenticationFailureHandler())
                 .and()
                 .logout()
+                .permitAll()
+                .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK))
                 .invalidateHttpSession(true)
                 .deleteCookies("JSESSIONID");
     }
