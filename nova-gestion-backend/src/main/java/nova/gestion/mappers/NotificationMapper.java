6ff9976318dac715ccbcb1aab80854e88e5479da
Philippe Pitz Clairoux <philippepitz123@gmail.com>
2019-11-27 15:27:21 -0500

    added NotificationService

--- nova-gestion-backend/src/main/java/nova/gestion/mappers/NotificationMapper.java
+++ nova-gestion-backend/src/main/java/nova/gestion/mappers/NotificationMapper.java
@@ -1,9 +1,13 @@
 package nova.gestion.mappers;
 
 import nova.gestion.model.Notification;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 
+@Mapper
+@Repository
 public interface NotificationMapper {
 
     ArrayList<Notification> getNotViewedNotifications();
---
+++ nova-gestion-backend/src/main/java/nova/gestion/services/NotificationService.java
@@ -0,0 +1,36 @@
+package nova.gestion.services;
+
+import nova.gestion.errors.exceptions.InvalidRequest;
+import nova.gestion.mappers.NotificationMapper;
+import nova.gestion.model.Notification;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+
+@Service
+public class NotificationService {
+
+    private final NotificationMapper notificationMapper;
+
+    @Autowired
+    public NotificationService(NotificationMapper notificationMapper) {
+        this.notificationMapper = notificationMapper;
+    }
+
+    @Transactional
+    ArrayList<Notification> getNotifications() {
+        return notificationMapper.getNotViewedNotifications();
+    }
+
+    @Transactional
+    void updateNotification(Integer idNotification) {
+
+        if (idNotification == null)
+            throw new InvalidRequest("No id passed");
+
+        notificationMapper.updateToViewed(idNotification);
+    }
+
+}
