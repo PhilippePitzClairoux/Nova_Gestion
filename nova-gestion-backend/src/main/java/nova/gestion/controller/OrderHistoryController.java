f9ce27456fbd7aff4947811c6a5281d14a9c6aea
Philippe Pitz Clairoux <43589004+PhilippePitzClairoux@users.noreply.github.com>
2019-12-19 13:25:03 -0500

    Historique commandes (#84)
    * fixed encoding error
    
    * added order history table!
    
    * added controller
    
    * fixed bug
    
    * fixed controller so we can query a period of time using two timestamps

---
+++ nova-gestion-backend/src/main/java/nova/gestion/controller/OrderHistoryController.java
@@ -0,0 +1,47 @@
+package nova.gestion.controller;
+
+import nova.gestion.model.OrderHistoryData;
+import nova.gestion.services.OrderHistoryService;
+import nova.gestion.model.OrderHistory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class OrderHistoryController {
+
+    private final OrderHistoryService orderHistoryService;
+
+    @Autowired
+    public OrderHistoryController(OrderHistoryService orderHistoryService) {
+        this.orderHistoryService = orderHistoryService;
+    }
+
+    /**
+     * GET /v1/orderhistory/timestamp -> retourne la liste des orderHistory
+     *
+     * @return ArrayList {@link nova.gestion.model.OrderHistory}
+     */
+    @GetMapping("/v1/orderhistory/{timestamp}")
+    public ArrayList<OrderHistory> getAllOrderHistory(@PathVariable Timestamp timestamp) {
+        return orderHistoryService.selectFromTimestampOrderHistory(timestamp);
+    }
+
+    /**
+     * GET /v1/orderhistory/idBlank/timestamp -> retourne la liste des orderHistory pour le blank specefique
+     *
+     * @return ArrayList {@link nova.gestion.model.OrderHistory}
+     */
+    @GetMapping("/v1/orderhistory/{startTimestamp}/{endTimestamp}")
+    public Collection<OrderHistoryData> getAllOrderHistoryForBlankFromTimestamp(@PathVariable Timestamp startTimestamp, @PathVariable Timestamp endTimestamp) {
+        return orderHistoryService.selectFromTimestampToTimestampOrderHistoryForProduct(startTimestamp, endTimestamp);
+    }
+
+
+
+}
---
+++ nova-gestion-backend/src/main/java/nova/gestion/mappers/OrderHistoryMapper.java
@@ -0,0 +1,22 @@
+package nova.gestion.mappers;
+
+import nova.gestion.model.OrderHistory;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+@Mapper
+@Repository
+public interface OrderHistoryMapper {
+    OrderHistory selectOrderHistory(Integer idOrderHistory);
+
+    ArrayList<OrderHistory> selectFromTimestampOrderHistory(Timestamp startDate);
+
+    ArrayList<OrderHistory> selectFromTimestampOrderHistoryForProduct(OrderHistory product);
+
+    ArrayList<OrderHistory> selectFromTimestampToTimestampOrderHistory(Timestamp timestampStart, Timestamp timestampEnd);
+
+    void insertOrderHistory(OrderHistory order);
+}
